d2cfb1a47a76aa5d23f8dbdd1715360bfde69232
Ception <ception@live.ca>
2022-01-17 23:11:54 -0500

    - started implementing correct AES, credit: izarooni - made a couple structure adjustments and assembly changes.

---
+++ WonderKingNA/WonderKingNA/Network/AES.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using WonderKingNA.Tools;
+
+namespace WonderKingNA.Network {
+    internal class AES {
+        private static readonly byte[] key = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7 };
+        private static readonly byte[] encryptionKey = new byte[176];
+        private static readonly byte[] decryptionKey = new byte[176];
+
+        public AES() {
+            ExpandKey();
+            Log.ConsoleMessage("[AES] Cryptography Initialized!");
+        }
+
+        private static void ExpandKey() {
+            
+        }
+    }
+}
