95d8746a0c66cb69da6d12bf46966d4eac43decb
Ception <ception@live.ca>
2022-01-23 20:26:50 -0500

    - Added player jobs class

---
+++ WonderKingNA/WonderKingNA/Player/Classes.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WonderKingNA.Player {
+    internal class Classes {
+        public enum FirstJob {
+            NULL,
+            Swordsman = 1,
+            Mage,
+            Thief,
+            Scout,
+        }
+
+        public enum SecondJob {
+            Warrior = 5,
+            Knight, //6
+            Priest, //7	
+            Wizard, //8
+            Rogue, //9
+            Ninja, //10
+            Gunner, //11
+            Archer, //12
+        }
+
+        public enum ThirdJob {
+            Berserker = 13,
+            Pladin,
+            Saint, 
+            Warlock,	
+            Knave,
+            Assassin,
+            Gunslinger,
+            Ranger,
+        }
+
+
+        public enum FourthJob {
+            Juggernaut = 21,
+            Temple_Knight,
+            Exorcist,
+            Necromancer,
+            Raider,
+            Nightstalker,
+            Sharpshooter,
+            Beast_Keeper,
+        }
+    }
+}
--- WonderKingNA/WonderKingNA/Player/Player.cs
+++ WonderKingNA/WonderKingNA/Player/Player.cs
@@ -3,8 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static WonderKingNA.Player.Classes;
 
 namespace WonderKingNA.Player {
     internal class Player {
+        private string Name;
+        private byte Level;
+        private int UserID;
+        private short ID;
+        private short HP, MP;
+        private short MaxHP, MaxMP;
+        private short Str, Dex, Int, Luck, Vit, Wis;
+        private FirstJob Job1;
+        private SecondJob Job2;
+        private ThirdJob Job3;
+        private FourthJob Job4;
+        private short X, Y;
+        private short SP, AP;
+        private short Hair, Eyes;
+        private long Money;
+        private short MinAttack;
+        private short MaxAttack;
+        private short MinRanageAttack;
+        private short MaxRanageAttack;
+        private short Defense;
+        private short HitRate;
+        private short Evasion;
     }
 }
